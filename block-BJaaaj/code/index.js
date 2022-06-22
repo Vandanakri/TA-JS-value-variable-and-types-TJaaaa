01318695451f520db90bcc3f83f186e8439dbc02
vandana <vandana123annusah@gmail.com>
2022-06-22 22:26:32 +0530

    BJaaaj

--- block-BJaaaj/code/index.js
+++ block-BJaaaj/code/index.js
@@ -6,37 +6,36 @@ let lastName = "Stark";
 // Do the following:
 
 // Alert the value of the firstName.
-alert("Arya")
+alert(firstName);
 
 
 // Alert the value of the lastName.
-alert("Stark")
+alert(lastName)
 
 // Assign a new value of `"John"` to firstName.
-let firstName = "John";
+firstName = "John";
 
 // Assign a new value of `"Snow"` to lastName.
-let lastName = "Snow";
+ lastName = "Snow";
 
 // Alert the new value of `firstName` and `lastName` by concatenating them
-alert(firstName + lastName)
+alert(firstName + lastName);
 // Create a new varaible using `const` named `fullName` and assign the fullname using firstName and lastName seperated by space.
-const fullName = firstName + lastName
+const fullName = firstName + " " + lastName;
 
 // Declare three variables `name,` `newUser` and `isAdmin`.
-let name;
-let newUser;
-let isAdmin;
+let name, newUser, isAdmin;
+
 // Assign your name as the value of `name`.
-let nameV = "Vandana"
+nameV = "Vandana"
 // Assign the value of `name` to `newUser`.
-let newUser = nameV;
+newUser = nameV;
 // Set the value of `isAdmin` to `true`.
-let isAdmin = true;
+isAdmin = true;
 // Alert the values of all the three variables.
-alert("name");
-alert("newUser");
-alert("isAdmin");
+alert(nameV);
+alert(newUser);
+alert(isAdmin);
 
 // Assignment and re-assignment
 
@@ -46,20 +45,21 @@ var wiseMan = "Tyrion Lannister";
 // Follow instructions and do the following. In case of an error write the error in comment
 
 // Reassign the value of `wiseMan` to "Samwell Tarly"
-var wiseMan = "Samwell Tarly";
+wiseMan = "Samwell Tarly";
 
 // Declare a variable `userName` with value "Lysa Arryn"
 let userName = "Lysa Arryn";
 // Declare a variable named `oddNumber` and assign a value `57`.
 let oddNumber = "57";
 // Reassign the value of `oddNumber` to 61
-var oddNumber = "61;"
+oddNumber = "61;"
 
 // Mathematical Operation
 
 let amount = 4280;
 
 // Using mathematical operations find the solutions. `(+, -, \*, / , etc.)`
+
 
 // Declare a new variable `reducedAmount`. In it store the value that is 24 less than the value of amount.
 let reducedAmount = "amount-24";
